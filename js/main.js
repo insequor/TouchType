7fcc67ae3cd755702f1b4b6bd898d02d81d406f2
ozgur <ozgur@insequor.com>
2013-11-23 17:00:38 +0000

    first introduction of jquery'

--- js/main.js
+++ js/main.js
@@ -1,11 +1,46 @@
 /*global require*/
 "use strict";
 
-require(["TouchType"], function(TouchType) {
-    //This function is called when scripts/helper/util.js is loaded.
-    //If util.js calls define(), then this function is not fired until
-    //util's dependencies have loaded, and the util argument will hold
-    //the module value for "helper/util".
-    console.log("main.js is loaded");
-    console.log(TouchType);
+require.config({
+    baseUrl: '../',
+    //Below line ensures that js files will not be cached on client side, good for development
+    urlArgs: 'version=',  //+ (new Date()).getTime(),
+    paths: {
+        // the left side is the module ID,
+        // the right side is the path to
+        // the jQuery file, relative to baseUrl.
+        // Also, the path should NOT include
+        // the '.js' file extension. This example
+        // is using jQuery 1.9.0 located at
+        // js/lib/jquery-1.9.0.js, relative to
+        // the HTML page.
+        jquery: 'external/jquery-1.10.2'
+    }
+});
+
+require(['jquery', "js/TouchType"], function($, TouchType) {
+    var KeyCodeEnter = 13;
+    var KeyCodeSpace = 32;
+    
+    var edit = $('#id_edit');
+    edit.focus();
+    function onKeyPress(keyEvent){
+        var keyCode = keyEvent.keyCode;
+        var keyValue = String.fromCharCode(keyCode);
+        //console.log(keyValue + ': ' + keyCode);
+        var input = keyEvent.srcElement;
+        //edit field is in keyEvent.target
+        if(keyCode === KeyCodeSpace) {
+            edit.val('');
+            return false; //So space is not handled by the target
+        }
+        else if(keyCode === KeyCodeEnter) {
+            console.log('Pause/Stop...');
+            return false; //So enter is not handled by the target
+        }
+    }
+    
+    edit.on('keypress', onKeyPress);
+    
+    
 });
