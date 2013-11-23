db88f54002a43f0f319b7834fa92eba96f76b5e8
ozgur <ozgur@insequor.com>
2013-11-23 18:44:09 +0000

    added shuffling the words array

--- js/TouchType.js
+++ js/TouchType.js
@@ -2,6 +2,22 @@
 "use strict";
 
 define(['js/WordsEn'], function(Words){
+
+    //this shuffle code is taken from: http://bost.ocks.org/mike/shuffle/
+    function shuffle(array) {
+        var m = array.length, t, i;
+        // While there remain elements to shuffle
+        while (m) {
+            // Pick a remaining element
+            i = Math.floor(Math.random() * m--);
+            // And swap it with the current element.
+            t = array[m];
+            array[m] = array[i];
+            array[i] = t;
+        }
+        return array;
+    }
+
     var TouchType = {
         _name: 'TouchType',
         _author: 'Ozgur Yuksel',
@@ -10,23 +26,20 @@ define(['js/WordsEn'], function(Words){
         currentWordIndex : -1,
         
         init: function (selectedCharacters){
-            this.words = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
-            
             //Below regular expression finds the characters which are not in the given
             //selectedCharacters string
             var regexp = new RegExp('[^' + selectedCharacters + ']');
             this.words = Words.filter(function(word){return ! regexp.exec(word);});
+            this.words = shuffle(this.words);
             console.log(this.words.length + ' words found');
-            this.currentWordIndex = 0;
+            this.currentWordIndex = -1;
         },
         
         next: function(userEntry) {
             if(this.currentWordIndex >= 0 && userEntry !== this.words[this.currentWordIndex])
                 return this.words[this.currentWordIndex];
             
-            this.currentWordIndex ++;
-            if (this.currentWordIndex >= this.words.length)
-                this.currentWordIndex = 0;
+            this.currentWordIndex = Math.floor(Math.random() * this.words.length) + 1;
             return this.words[this.currentWordIndex];
         }
         
