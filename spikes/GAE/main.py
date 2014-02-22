403ff5f73df8d387368b58214be1a807d3e9da17
Ozgur <ozgur@insequor.com>
2014-02-22 13:33:44 +0200

    Google Apple Engine Files
    Not sure if we will use GAE but lets keep the files for reference

--- spikes/GAE/main.py
+++ spikes/GAE/main.py
@@ -14,12 +14,46 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from google.appengine.api import users
+    
 import webapp2
+import json
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        text = 'Hi there<br />'
+        if 1:
+            req = self.request
+            
+            for attr in dir(req):
+                if attr.startswith('_'): 
+                    continue
+                text += attr
+                text += ' = '
+                try: 
+                    text += str(getattr(req, attr))
+                except: 
+                    text += 'NA'
+                text += '<br />\n'
+        self.response.write(text)
 
+class UserHandler(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            self.response.headers['Content-Type'] = 'text/plain'
+            #self.response.write('Hello, ' + user.nickname())
+            obj = {
+                'user': user.nickname()
+                , 'id': user.user_id()}
+            text = json.dumps(obj, indent=1, sort_keys=False)
+            self.response.write(text)
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
+        
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/user/*.*', UserHandler),
+    ('/*.*', MainHandler),
 ], debug=True)
